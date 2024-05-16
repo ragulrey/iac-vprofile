fd2f5e195ad26ca7f6c17247fe44048470aaf2eb
ragulrey <ragulkannan6995@gmail.com>
2024-05-16 23:23:40 +0100

    Bucket name updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "gitops-vproeks"
+  default     = "gitops-eks"
 }
 
 ##
